fcf1ef3680cf059feda1c235c3b7b6c775394d6c
gerolomo <felipegerolomo@gmail.com>
2020-09-21 20:37:02 -0300

    responsividade

--- src/app/modules/playlist/playlist.component.ts
+++ src/app/modules/playlist/playlist.component.ts
@@ -4,20 +4,29 @@ import { SpotifyService } from 'src/app/services/spotifyService/spotify.service'
 import _ from 'lodash';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogMusicSearchComponent } from 'src/app/components/dialog-music-search/dialog-music-search.component';
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
   styleUrls: ['./playlist.component.sass']
 })
 export class PlaylistComponent implements OnInit {
+  isSmallScreen = this.breakpointObserver.isMatched('(max-width: 425px)');
   defaultPlaylistImage = 'assets/images/default-playlist.png';
 
   idPlaylist: string;
   playlist: any;
 
-  constructor(private actRoute: ActivatedRoute, private spotifyService: SpotifyService, public dialog: MatDialog) {
+  constructor(
+    private actRoute: ActivatedRoute,
+    private spotifyService: SpotifyService,
+    public dialog: MatDialog,
+    private breakpointObserver: BreakpointObserver
+  ) {
     this.idPlaylist = this.actRoute.snapshot.params.id;
     this.getPlaylist();
+    console.log('s', this.isSmallScreen);
   }
 
   ngOnInit(): void {
@@ -41,7 +50,7 @@ export class PlaylistComponent implements OnInit {
 
   openDialogMusics() {
     const dialogRef = this.dialog.open(DialogMusicSearchComponent, {
-      width: '600px',
+      minWidth: this.isSmallScreen ? '100%' : '600px',
       maxHeight: '600px',
       data: {
         songs: this.playlist.tracks.items.map((item) => { return item.track }),
